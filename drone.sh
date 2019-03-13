5c31f32d32e1eb37d2a8cb2241c3b31e078c7523
Adam Szkoda <adaszko@gmail.com>
2019-03-13 07:38:30 +0100

    Add safeguard against overwriting SSH keys

--- drone.sh
+++ drone.sh
@@ -11,6 +11,11 @@ ssh-master () {
 
 
 main () {
+    if [[ -z ${DRONE:-} ]]; then
+        echo This script is intended to be run by the Drone CI only
+        return 1
+    fi
+
     mkdir --parents ~/.ssh
     echo -n "$SSH_KEY_B64" | base64 --decode >~/.ssh/id_rsa
     echo "$SSH_HOST ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBIl3+HjRHR4BzepYjIik/ORKKjFdU0Lv5QuYoVAKPsYBsfQUvhvnXzLlRLBXqokCfPWT+VPGMh114G6Vi0pjsR8=" >~/.ssh/known_hosts
