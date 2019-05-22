7df96297ba088e8197a3faf8b0263b4716754d5f
Adam Szkoda <adaszko@gmail.com>
2019-05-22 12:54:02 +0200

    Disable SSH host checking
    The machine identity changes too often.

--- drone.sh
+++ drone.sh
@@ -6,7 +6,7 @@ set -o pipefail
 
 
 ssh-master () {
-    ssh -i ~/.ssh/id_rsa ${SSH_USERNAME}@${SSH_HOST} "$@"
+    ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa ${SSH_USERNAME}@${SSH_HOST} "$@"
 }
 
 
@@ -18,7 +18,6 @@ main () {
 
     mkdir --parents ~/.ssh
     echo -n "$SSH_KEY_B64" | base64 --decode >~/.ssh/id_rsa
-    echo "$SSH_HOST ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBJ8oF4/NmXO+tgnh66pNW5y5nEYj04e9N3BUj/8TcAOeq0llhzKJwpBw6uaDRmYzw53IyAW2aG9c73x5Ozp0a+I=" >~/.ssh/known_hosts
     chmod --recursive go-rwx ~/.ssh
 
     ssh-master rm --recursive --force whiteblock
